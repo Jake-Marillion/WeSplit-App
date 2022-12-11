e563df2a612339b43bf03f79f9f38865c16dadec
Jacob Marillion <110437842+Jake-Marillion@users.noreply.github.com>
2022-12-10 19:56:53 -0700

    Add files via upload

---
+++ WeSplit_100UI_APP/WeSplit100Days/ContentView.swift
@@ -0,0 +1,99 @@
+//
+//  ContentView.swift
+//  WeSplit100Days
+//
+//  Created by Jacob Marillion on 7/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = [0, 10, 15, 20, 25]
+    
+//    let moneyCode = format: .currency(code: Locale.current.currencyCode ?? "USD")
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+        
+    }
+    var totalWithTip: Double {
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        
+        return grandTotal
+    }
+    
+    
+    var body: some View {
+        
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<30) {
+                            Text("\($0) People")
+                        }
+                    }
+                } header: {
+                    Text("Bill Amount")
+                }
+                
+                Section {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Tip?")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                } header: {
+                    Text("Total Per Person")
+                }
+                Section {
+                    Text(totalWithTip, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                } header: {
+                    Text("Total With Tip")
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit_100UI_APP/WeSplit100Days/WeSplit100DaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit100DaysApp.swift
+//  WeSplit100Days
+//
+//  Created by Jacob Marillion on 7/16/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit100DaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
